d50f36f57efddc253e0c536ad0ff497c14b638a5
yugmani <yugmani@yahoo.com>
2022-07-24 11:46:10 -0700

    testing truthy values

--- index.js
+++ index.js
@@ -1,2 +1,65 @@
 // Import stylesheets
 import './style.css';
+
+// *********************************************
+// Refactoring Javascript Code: Truthy and Falsy
+// *********************************************
+
+let b;
+console.log(typeof b); //undefined
+
+if (b != undefined) {
+  console.log('this is b: ', b);
+} else {
+  console.log('b is empty');
+}
+//b is empty
+
+//if b has a value - and is therefore not undefined - we want to print the value to to console. Otherwise, if b does not have a value, then we want to print the statement "b is empty".
+
+//refactor the same code in more elegant and less redundant way
+if (b) {
+  console.log('this is b: ', b);
+} else {
+  console.log('b is empty');
+}
+//b is empty
+// In a boolean context, the value of undefined will always return false.
+// When we write (b != undefined) we are really asking if b is not false. Alternatively, is b true?
+
+b = true;
+if (b) {
+  console.log('this is b: ', b);
+} else {
+  console.log('b is empty');
+}
+// this is b: true
+
+b = false;
+if (b) {
+  console.log('this is b: ', b);
+} else {
+  console.log('b is empty');
+}
+//b is empty
+
+b = '';
+if (b) {
+  console.log('this is b: ', b);
+} else {
+  console.log('b is empty');
+}
+//b is empty
+
+b = 'Kyle';
+if (b) {
+  console.log('this is b: ', b);
+} else {
+  console.log('b is empty');
+}
+//this is b: Kyle
+
+//Takeout
+// *** When b is placed in boolean context, the value of b becomes true.
+// *** Numbers, integers, and objects are what we call truthy values.
+// *** Truthy values are values that return true when they are placed in boolean contexts.
